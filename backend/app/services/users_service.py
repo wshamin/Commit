280bffb0a65245a8441a0012601c2959c365372d
wshamin <weshamin@gmail.com>
2023-08-05 14:49:22 +0300

    Refactor create_user function to use async MongoDB insert_one method

--- backend/app/services/users_service.py
+++ backend/app/services/users_service.py
@@ -4,7 +4,7 @@ from ..db.database import user_collection
 
 
 async def create_user(user_data: Dict[str, Any]) -> Dict[str, Any]:
-    result = user_collection.insert_one(user_data)
+    result = await user_collection.insert_one(user_data)
     new_user_id = result.inserted_id
-    new_user = user_collection.find_one({"_id": new_user_id})
+    new_user = await user_collection.find_one({"_id": new_user_id})
     return new_user
