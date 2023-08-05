c19cca470ffd6667fc2dd185587105a29789c4eb
wshamin <weshamin@gmail.com>
2023-08-05 14:28:16 +0300

    Refactored code to replace PyObjectId with native string type for MongoDB ID handling

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,56 +1,38 @@
 from pydantic import BaseModel, Field
-from bson import ObjectId
 from typing import List, Optional
 
 
-# Создаем обертку для поля _id в MongoDB, т.к. Python не умеет работать с типом данных ObjectId по умолчанию
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError('Invalid ObjectId')
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type='string')
-
-
 class Material(BaseModel):
-    id: Optional[PyObjectId] = Field(alias='_id')
+    id: Optional[str] = Field(alias='_id')
     filename: str
     filetype: str
     url: str
 
 
 class User(BaseModel):
-    id: Optional[PyObjectId] = Field(alias='_id')
+    id: Optional[str] = Field(alias='_id')
     email: str
     password: str
     name: str
     role: str
-    courses: List[PyObjectId]
+    courses: List[str]
 
 
 class Course(BaseModel):
-    id: Optional[PyObjectId] = Field(alias='_id')
+    id: Optional[str] = Field(alias='_id')
     title: str
     description: str
     materials: List[Material]
     cost: float
-    owner: PyObjectId
-    students: List[PyObjectId]
-    reviews: List[PyObjectId]
+    owner: str
+    students: List[str]
+    reviews: List[str]
 
 
 class Review(BaseModel):
-    id: Optional[PyObjectId] = Field(alias='_id')
-    course: PyObjectId
-    user: PyObjectId
+    id: Optional[str] = Field(alias='_id')
+    course: str
+    user: str
     text: str
     rating: int
     response: str
