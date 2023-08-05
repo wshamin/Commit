ccc6258afc4f5f9f38e262e709eef026df94d745
wshamin <weshamin@gmail.com>
2023-08-05 22:50:52 +0300

    Added CRUD operations for the User model in the users_service module. Implemented a testing mechanism in the main.py file to validate the CRUD functionality.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 import asyncio
-from .services.users_service import create_user
+from .services.users_service import (
+    create_user,
+    get_all_users,
+    get_user_by_id,
+    update_user,
+    delete_user
+)
 
 
-def main():
+async def run_tests():
+    # Создаем пользователя
     test_user_data = {
         "email": "testuser@email.com",
         "password": "securepassword",
@@ -10,9 +17,32 @@ def main():
         "role": "user",
         "courses": []
     }
+    new_user = await create_user(test_user_data)
+    print("Created user:", new_user)
 
-    new_user = asyncio.run(create_user(test_user_data))
-    print(new_user)
+    # Извлекаем всех пользователей
+    all_users = await get_all_users()
+    print("All users:", all_users)
+
+    # Извлекаем пользователя по ID
+    user_id = new_user['_id']
+    retrieved_user = await get_user_by_id(user_id)
+    print(f"Retrieved user with ID {user_id}:", retrieved_user)
+
+    # Обновляем пользователя
+    update_data = {
+        "name": "Updated Test User"
+    }
+    updated_user = await update_user(user_id, update_data)
+    print(f"Updated user with ID {user_id}:", updated_user)
+
+    # Удаляем пользователя
+    deleted_result = await delete_user(user_id)
+    print(f"Deleted user with ID {user_id}:", deleted_result)
+
+
+def main():
+    asyncio.run(run_tests())
 
 
 if __name__ == "__main__":
--- backend/app/services/users_service.py
+++ backend/app/services/users_service.py
@@ -1,5 +1,5 @@
-from typing import Dict, Any
-from ..db.models import User, Course, Review
+from typing import Dict, Any, List, Union, Optional
+from ..db.models import User
 from ..db.database import user_collection
 
 
@@ -13,5 +13,39 @@ async def create_user(user_data: Dict[str, Any]) -> Dict[str, Any]:
 
     result = await user_collection.insert_one(user_data)
     new_user_id = result.inserted_id
-    new_user = await user_collection.find_one({"_id": new_user_id})
+    new_user = await user_collection.find_one({'_id': new_user_id})
     return new_user
+
+
+async def get_user_by_id(user_id: str) -> Union[dict, None]:
+    user = await user_collection.find_one({'_id': user_id})
+    return user
+
+
+async def get_all_users() -> List[User]:
+    users = []
+    async for user in user_collection.find():
+        users.append(user)
+    return users
+
+
+async def update_user(user_id: str, user_data: Dict[str, Any]) -> Union[dict, None]:
+    if not user_data:
+        return None
+    user_in_db = await user_collection.find_one({'_id': user_id})
+    if not user_in_db:
+        return None
+
+    user_data = User(**user_data).model_dump(exclude_unset=True)
+    updated_user = await user_collection.update_one({'_id': user_id}, {'$set': user_data})
+    if updated_user:
+        return await user_collection.find_one({'_id': user_id})
+    return None
+
+
+async def delete_user(user_id: str) -> Union[dict, None]:
+    user = await user_collection.find_one({'_id': user_id})
+    if user:
+        await user_collection.delete_one({'_id': user_id})
+        return user
+    return None
