ec69ebce872217760be3e27a5eb4920a6ed4cb4a
wshamin <weshamin@gmail.com>
2023-08-05 16:05:26 +0300

    Added Pydantic validation for user creation and excluded None fields before database insertion

--- backend/app/services/users_service.py
+++ backend/app/services/users_service.py
@@ -4,6 +4,13 @@ from ..db.database import user_collection
 
 
 async def create_user(user_data: Dict[str, Any]) -> Dict[str, Any]:
+    # Создаем экземпляр модели User для валидации через Pydantic
+    user = User(**user_data)
+
+    # Преобразуем модель User обратно в словарь, для корректного присваивания
+    # уникального _id в MongoDB
+    user_data = user.model_dump(exclude_none=True)
+
     result = await user_collection.insert_one(user_data)
     new_user_id = result.inserted_id
     new_user = await user_collection.find_one({"_id": new_user_id})
