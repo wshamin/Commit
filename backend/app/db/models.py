8658d711d9bcce614cd4fe798453aaee645b1e82
wshamin <weshamin@gmail.com>
2023-08-10 01:27:13 +0300

    Refactor: Simplified ObjectId handling in Pydantic models.

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -2,32 +2,39 @@ from typing import List, Optional
 from typing_extensions import Annotated
 from pydantic import Field, BaseModel
 from pydantic.functional_validators import AfterValidator
-from bson import ObjectId as _ObjectId
+import pydantic.json
+from bson import ObjectId
+
+# Исправляет ошибку сериализации ObjectID для Pydantic
+pydantic.json.ENCODERS_BY_TYPE[ObjectId] = str
 
 
-def check_object_id(value: str) -> str:
-    if not _ObjectId.is_valid(value):
-        raise ValueError('Invalid ObjectId')
-    return value
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
-
-ObjectId = Annotated[str, AfterValidator(check_object_id)]
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid ObjectId')
+        return str(v)
 
 
 class Material(BaseModel):
-    id: Optional[ObjectId] = Field(alias='_id')
+    id: Optional[PyObjectId] = Field(default=None, alias='_id')
     filename: str
     filetype: str
     url: str
 
 
 class User(BaseModel):
-    id: Optional[ObjectId] = Field(default=None, alias='_id')
+    id: Optional[PyObjectId] = Field(default=None, alias='_id')
     email: str
     password: str
     name: str
     role: str
-    courses: List[ObjectId]
+    courses: List[str]
 
 
 class UserUpdate(BaseModel):
@@ -35,24 +42,24 @@ class UserUpdate(BaseModel):
     password: Optional[str] = None
     name: Optional[str] = None
     role: Optional[str] = None
-    courses: Optional[List[ObjectId]] = None
+    courses: Optional[List[str]] = None
 
 
 class Course(BaseModel):
-    id: Optional[ObjectId] = Field(alias='_id')
+    id: Optional[PyObjectId] = Field(default=None, alias='_id')
     title: str
     description: str
-    materials: List[Material]
+    materials: List[str]
     cost: float
     owner: ObjectId
-    students: List[ObjectId]
-    reviews: List[ObjectId]
+    students: List[str]
+    reviews: List[str]
 
 
 class Review(BaseModel):
-    id: Optional[ObjectId] = Field(alias='_id')
-    course: ObjectId
-    user: ObjectId
+    id: Optional[PyObjectId] = Field(default=None, alias='_id')
+    course: str
+    user: str
     text: str
     rating: int
     response: str
