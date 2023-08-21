d60fcb49ddb6413fb6c8206945c48669ea014c2e
wshamin <weshamin@gmail.com>
2023-08-21 22:40:15 +0300

    Added jsonable_encoder import and integrated into student creation route

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,49 +1,13 @@
-from fastapi import APIRouter, HTTPException, Body
+from fastapi import APIRouter, Body
+from fastapi.encoders import jsonable_encoder
+from ...db.models import UserModel, UpdateUserModel
+from ...db.database import user_collection as db
 
-from ...services.users_service import (
-    create_user,
-    get_user_by_id,
-    get_all_users,
-    update_user,
-    delete_user
-)
-
-from ...db.models import User, UserUpdate
-
-router = APIRouter()
+app = APIRouter()
 
 
-@router.post('/users/', response_model=User)
-async def create_new_user(user: User = Body(...)):
-    user_data = user.model_dump(exclude_unset=True)
-    new_user = await create_user(user_data)
+@app.post('/users/', response_description="Add new user", response_model=UserModel)
+async def create_new_user(user: UserModel = Body(...)):
+    user = jsonable_encoder(user)
+    new_user = await db["students"].insert_one(user)
     return new_user
-
-
-@router.get('/users/{user_id}', response_model=User)
-async def get_single_user(user_id: str):
-    user = await get_user_by_id(user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail='User not found')
-    return user
-
-
-@router.get('/users/', response_model=list[User])
-async def get_users_list():
-    return await get_all_users()
-
-
-@router.put('/users/{user_id}', response_model=User)
-async def modify_user(user_id: str, user: UserUpdate = Body(...)):
-    user_updated = await update_user(user_id, user.model_dump(exclude_unset=True))
-    if not user_updated:
-        raise HTTPException(status_code=404, detail='User not found')
-    return user_updated
-
-
-@router.delete('/users/{user_id}', response_model=User)
-async def remove_user(user_id: str):
-    deleted_user = await delete_user(user_id)
-    if not deleted_user:
-        raise HTTPException(status_code=404, detail='User not found')
-    return deleted_user
--- backend/app/services/users_service.py
+++ backend/app/services/users_service.py
@@ -1,47 +1,48 @@
-from typing import Dict, Any, List, Union, Optional
-from ..db.models import User, UserUpdate
-from ..db.database import user_collection
-
-
-async def create_user(user_data: Dict[str, Any]) -> Dict[str, Any]:
-    user = User(**user_data)
-    user_data = user.model_dump(exclude_none=True)
-
-    result = await user_collection.insert_one(user_data)
-    new_user_id = result.inserted_id
-    new_user = await user_collection.find_one({'_id': new_user_id})
-    return new_user
-
-
-async def get_user_by_id(user_id: str) -> Union[dict, None]:
-    user = await user_collection.find_one({'_id': user_id})
-    return user
-
-
-async def get_all_users() -> List[User]:
-    users = []
-    async for user in user_collection.find():
-        users.append(user)
-    return users
-
-
-async def update_user(user_id: str, user_data: Dict[str, Any]) -> Union[dict, None]:
-    if not user_data:
-        return None
-    user_in_db = await user_collection.find_one({'_id': user_id})
-    if not user_in_db:
-        return None
-
-    user_data = UserUpdate(**user_data).model_dump(exclude_none=True)
-    updated_user = await user_collection.update_one({'_id': user_id}, {'$set': user_data})
-    if updated_user:
-        return await user_collection.find_one({'_id': user_id})
-    return None
-
-
-async def delete_user(user_id: str) -> Union[dict, None]:
-    user = await user_collection.find_one({'_id': user_id})
-    if user:
-        await user_collection.delete_one({'_id': user_id})
-        return user
-    return None
+# from typing import Dict, Any, List, Union, Optional
+# from ..db.models import User, UserUpdate
+# from ..db.database import user_collection
+#
+#
+# async def create_user(user_data: Dict[str, Any]) -> Dict[str, Any]:
+#     # user = User(**user_data)
+#     # user_data = user.model_dump(exclude_none=True)
+#     #
+#     # result = await user_collection.insert_one(user_data)
+#     # new_user_id = result.inserted_id
+#     # new_user = await user_collection.find_one({'_id': new_user_id})
+#
+#     return new_user
+#
+#
+# async def get_user_by_id(user_id: str) -> Union[dict, None]:
+#     user = await user_collection.find_one({'_id': user_id})
+#     return user
+#
+#
+# async def get_all_users() -> List[User]:
+#     users = []
+#     async for user in user_collection.find():
+#         users.append(user)
+#     return users
+#
+#
+# async def update_user(user_id: str, user_data: Dict[str, Any]) -> Union[dict, None]:
+#     if not user_data:
+#         return None
+#     user_in_db = await user_collection.find_one({'_id': user_id})
+#     if not user_in_db:
+#         return None
+#
+#     user_data = UserUpdate(**user_data).model_dump(exclude_none=True)
+#     updated_user = await user_collection.update_one({'_id': user_id}, {'$set': user_data})
+#     if updated_user:
+#         return await user_collection.find_one({'_id': user_id})
+#     return None
+#
+#
+# async def delete_user(user_id: str) -> Union[dict, None]:
+#     user = await user_collection.find_one({'_id': user_id})
+#     if user:
+#         await user_collection.delete_one({'_id': user_id})
+#         return user
+#     return None
