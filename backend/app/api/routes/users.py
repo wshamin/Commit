5be54a0b23f5a94a8a22e90ac26e8bc9dce6cb26
wshamin <weshamin@gmail.com>
2023-08-21 22:42:19 +0300

    Added jsonable_encoder import and integrated into student creation route v. 2

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Body
 from fastapi.encoders import jsonable_encoder
-from ...db.models import UserModel, UpdateUserModel
+from ...db.models import UserModel
 from ...db.database import user_collection as db
 
-app = APIRouter()
+router = APIRouter()
 
 
-@app.post('/users/', response_description="Add new user", response_model=UserModel)
+@router.post('/users/', response_description="Add new user", response_model=UserModel)
 async def create_new_user(user: UserModel = Body(...)):
     user = jsonable_encoder(user)
     new_user = await db["students"].insert_one(user)
