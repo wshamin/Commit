619cfc4b5e20d73e82be9e946d08b7ed6924d050
Виктор Шамин <wshamin@wmac.local>
2023-08-23 19:39:01 +0300

    Updated API base URL to use HTTPS via Nginx proxy (v.7)

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(router, tags=["users"])
+app.include_router(router, prefix="/api", tags=["users"])
 
 def custom_openapi():
     if app.openapi_schema:
