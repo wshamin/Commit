27355847000926e6c78cb630ddfee26459d1f95a
Виктор Шамин <wshamin@wmac.local>
2023-08-23 23:22:55 +0300

    Fix SwaggerUI display issue by customizing OpenAPI schema (v.2)

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.openapi.utils import get_openapi
 from .api.routes.users import router
 
-app = FastAPI(docs_url="/api/docs")
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 
 origins = [
     "http://127.0.0.1:3000", 
