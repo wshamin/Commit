36a80794c7aa1786376d7203fbd34c00063b78ad
wshamin <weshamin@gmail.com>
2023-08-25 20:09:18 +0300

    Added login button next to the register button. (v.6)

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from ...db.database import user_collection
-from ...db.models import User, Token, TokenData
-from ...core.config import settings
-from ...core.security import verify_password, get_password_hash, create_access_token
-from ...schema.schemas import users_to_dict_list
+from datetime import timedelta
+
 from bson import ObjectId
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from ...core.config import settings
+from ...core.security import create_access_token, verify_password
+from ...db.database import user_collection
+from ...db.models import User
+from ...schema.schemas import users_to_dict_list
 
 
 router = APIRouter()
