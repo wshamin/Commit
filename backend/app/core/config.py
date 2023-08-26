40b3c388bc75ee27f062c6b3776d962d83903ae9
Виктор Шамин <wshamin@wmac.local>
2023-08-27 01:13:09 +0300

    Added SECRET_KEY to Settings for JWT authentication.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    SECRET_KEY: str
 
     class Config:
         env_file = ".env"
