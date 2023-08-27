581d1009e2c488f171d9f8857425a2b9a556bfab
Виктор Шамин <wshamin@wmac.local>
2023-08-27 23:51:12 +0300

    Implement training creation and data transformation functions (v.16)

--- frontend/src/components/CreateTraining.js
+++ frontend/src/components/CreateTraining.js
@@ -27,8 +27,8 @@ function CreateTraining() {
             };
 
             const response = await axios.post(`${process.env.REACT_APP_API_URL}trainings/`, dataToSend);
-            print(response.status);
-            if (response.status === 200) {
+
+            if (response.status === 201) {
                 alert('Тренинг успешно создан!');
                 navigate(`/trainings/${response.data.id}`);
             } else {
