ceb3dc10baa290b6d43186418c937fc4b3076275
wshamin <weshamin@gmail.com>
2023-08-27 14:16:36 +0300

    Refactored config to load environment variables from .env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,13 @@
+import os
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
 
 
 class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    SECRET_KEY: str
+    SECRET_KEY: str = os.environ.get('SECRET_KEY')
 
     class Config:
-        env_file = ".env"
-
-
-settings = Settings()
+        env_file = "../.env"
