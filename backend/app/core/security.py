c00c66b30b51f67af18b0cb64b5c1bd30b8184cb
wshamin <weshamin@gmail.com>
2023-08-29 21:20:29 +0300

    Refactored ALGORITHM definition to centralize in config.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,14 +3,12 @@ from datetime import datetime, timedelta
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
-from .config import settings
+from .config import settings, ALGORITHM
 from ..db.models import User, TokenData
 from ..db.database import user_collection
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-ALGORITHM = "HS256"
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token/")
 
@@ -40,7 +38,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
