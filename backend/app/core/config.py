9f835762e08532091f98b77495062370a2307f1f
wshamin <weshamin@gmail.com>
2023-08-29 21:22:04 +0300

    Refactored ALGORITHM definition to centralize in config.py (v.2)

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,7 +8,7 @@ load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
 class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     SECRET_KEY: str = os.environ.get('SECRET_KEY')
-    ALGORITHM = "HS256"
+    ALGORITHM: str = "HS256"
 
     class Config:
         env_file = "../.env"
