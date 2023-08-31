5c0dd5ad2101cdf3ab8be358341847bddf8792b8
wshamin <weshamin@gmail.com>
2023-08-31 21:49:36 +0300

    Removed trainings get request from App.js (v.2)

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Dashboard({ trainings }) {
+function Dashboard() {
     const [trainings, setTrainings] = useState([]);
 
     useEffect(() => {
